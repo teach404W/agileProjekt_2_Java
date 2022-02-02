c2f8b51ef6d27fc7499737afa39ddc675db1870d
Felix Wienands <felix.wienands@hebk.de>
2022-02-02 13:56:48 +0100

    update classes

--- Java/src/Schuelerverwaltung/src/Schueler.java
+++ Java/src/Schuelerverwaltung/src/Schueler.java
@@ -1,3 +1,27 @@
-public class Schueler {
+public class Schueler extends Person{
+    private String password = "null";
+
+    public Schueler(String pVorname, String pNachname, int pAlter, String pPassword){
+        super(pVorname, pNachname, pAlter);
+        setPassword(pPassword);
+    }
+
+
+    public void setPassword(String pPassword){
+        if (pPassword.length() < 4){
+            System.out.println("Passwort kann nicht geändert werden -- zu kurz");
+        }else {
+            this.password = pPassword;
+            System.out.println("Passwort erfolgreich geändert");
+        }
+    }
+
+    public boolean ueberPruefePassword(String pPassword){
+        if (this.password.equals(pPassword)){
+            return true;
+        }else {
+            return false;
+        }
+    }
     
 }
